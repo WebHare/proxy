bbfd16b4038e5a676a677e621cdb690447a7d7f2
Arnold Hendriks <arnold@webhare.nl>
2019-03-01 12:34:32 +0100

    Don't push scheduled rebuilds

--- build.sh
+++ build.sh
@@ -13,3 +13,10 @@ if [ "$1" == "--push" ]; then
   docker push $TAG
   echo Pushed: $TAG
 fi
+
+if [ "$1" == "--push-if-pushed" ]; then
+  if [ "$CI_PIPELINE_SOURCE" == "push" ]; then
+    push=1
+  fi
+  shift
+fi
