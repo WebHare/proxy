54c85ee55b8af39ad081e6b06efd85b63fa123c1
Arnold Hendriks <arnold@webhare.nl>
2020-01-02 22:01:27 +0100

    Fix build tag

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ cd ${BASH_SOURCE%/*}
 
 # WebHare/SV integration
 
-export TAG="gitlab-registry.webhare.com/webhare/proxy:devbuild"
+export TAG="webhare/proxy:devbuild"
 if ! ./docker.sh build ; then
   echo Build failed
   exit
