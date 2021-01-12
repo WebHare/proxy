515786b81f9812b7dc87c02cd4906e6391eb17f5
Arnold Hendriks <arnold@webhare.nl>
2021-01-12 19:26:01 +0100

    autorequest certbot certificate

--- dropins/opt/container/launch.sh
+++ dropins/opt/container/launch.sh
@@ -4,4 +4,8 @@ mkdir -p /opt/webhare-proxy-data/log
 mkdir -p /opt/webhare-proxy-data/letsencrypt/{etc,lib,log}
 chown -R root.root /opt/webhare-proxy-data/log # to make sure logrotate doesn't freak out about the permissions
 
+if [ -n "$WEBHAREPROXY_ADMINHOSTNAME" -a -n "$WEBHAREPROXY_LETSENCRYPTEMAIL" -a ! -d /etc/letsencrypt/live/$WEBHAREPROXY_ADMINHOSTNAME ]; then
+  /opt/container/try-adminhost-certbot.sh & disown
+fi
+
 exec /sbin/my_init
---
+++ dropins/opt/container/try-adminhost-certbot.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo -e "\n[try-adminhost-certbot] Waiting for $WEBHAREPROXY_ADMINHOSTNAME to come online\n"
+
+CONNECTED=
+for i in $(seq 1 10); do
+  if ( curl -s http://$WEBHAREPROXY_ADMINHOSTNAME/ | grep --silent "adminhost" ); then
+    CONNECTED=1
+    break;
+  fi
+  echo -n "."
+  sleep 1
+done
+
+if [ -z "$CONNECTED" ]; then
+  echo Failed to connect. Sleeping for 1 hour and then retrying
+  sleep 3600
+  exec $0
+  exit 1
+fi
+
+echo -e "\n[try-adminhost-certbot] PROXY IS ONLINE!\n"
+/opt/container/setup-adminhost-certbot.sh
+
+if [ ! -d /etc/letsencrypt/live/$WEBHAREPROXY_ADMINHOSTNAME ] ; then
+  echo -e "\n[try-adminhost-certbot] No keyfile found, will retry in an hour\n"
+  sleep 3600
+  exec $0
+  exit 1
+fi
